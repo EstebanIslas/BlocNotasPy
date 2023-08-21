1e9cfcb6b8a4f4affcab57902c001549d7bdd6fe
EstebanIslas <1717110261@utectulancingo.edu.mx>
2023-08-21 11:25:37 -0600

    Maqueta del asistente

--- main.py
+++ main.py
@@ -4,3 +4,24 @@ Proyecto creado en MVC backend
 -Login y Registro
 -Creación, Edición, Eliminado y Listas de datos
 """
+
+print("""
+Acciones Disponibles:
+    - Registro
+    - Login
+""")
+
+accion = str(input("Ingresa el comando que desee utilizar: "))
+
+if accion.lower() == "registro":
+    print("\nAccion de registro")
+    name = str(input("Ingresa el nombre: "))
+    last_name = str(input("Ingresa tus apellidos: "))
+    email = str(input("Ingresa tu email: "))
+    password = str(input("Ingresa tu contraseña: "))
+
+
+elif accion.lower() == "login":
+    print("\nAccion de login")
+    email = str(input("Ingresa tu email: "))
+    password = str(input("Ingresa tu contraseña: "))
